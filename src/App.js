465a32018fd249a8a1f90d6333d7c2ba954b2a58
Yaoyi Mi <yaoyi.mi@brampton.ca>
2023-12-21 14:40:19 -0500

    changes made from the shopping cart lab (module 4) in react course

--- src/App.js
+++ src/App.js
@@ -8,8 +8,34 @@ import Location from './components/Location';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <AppProvider>
+        <div className="container">
+            <h1 className='mt-3'>Shopping App</h1>
+            <div className='row mt-3'>
+                <div className='col-sm'>
+                    <CartValue />
+                </div>
+
+                <div className='col-sm'>
+                    <Location />
+                </div>
+            </div>
+
+            <div className='row mt-3'>
+                <h3 className='mt-3'>Shopping Cart</h3>
+                <div className='col-sm'>
+                    <ExpenseList />
+                </div>
+            </div>
+
+            <div className='row mt-3'>
+                <h3 className='mt-3'>Add Items</h3>
+                <div className='col-sm'>
+                    <ItemSelected />
+                </div>
+            </div>
+        </div>
+    </AppProvider>
   );
 }
 
